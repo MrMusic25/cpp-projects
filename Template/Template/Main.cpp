19fedf43fb0d64af18f62255a26d19a2cae73f1c
Kyle Krattiger <mr.music25@gmail.com>
2016-10-05 18:08:44 -0700

    Created template project for future use

---
+++ Template/Template/Main.cpp
@@ -0,0 +1,28 @@
+// Template file to be used for most C++ projects
+// GOTO: Directory containing folder, and copy this folder to a new folder of the same name
+// RENAME the .sln file to initialize new project, then open in VS
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* * * * * * * * * * *
+ *  Global Variables *
+ * * * * * * * * * * */
+
+
+
+/* * * * * * * * * * * *
+ * Function Prototypes *
+ * * * * * * * * * * * */
+
+
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+	return 0;
+}
+
+/* * * * * * *
+ * Functions *
+ * * * * * * */
