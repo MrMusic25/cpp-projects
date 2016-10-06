999da7d260b538f3b36fd17cae84f539458f1e86
Kyle Krattiger <mr.music25@gmail.com>
2016-10-05 19:32:39 -0700

    Changed the usage

--- Template/Template/Main.cpp
+++ Template/Template/Main.cpp
@@ -1,6 +1,5 @@
 // Template file to be used for most C++ projects
-// GOTO: Directory containing folder, and copy this folder to a new folder of the same name
-// RENAME the .sln file to initialize new project, then open in VS
+// USAGE: Create a new project, and then copy over this Main.cpp file. Rename it accordingly
 #include <iostream>
 #include <string>
 using namespace std;
