a8707bd6e00e1dfcd295eedc3b9e96e17cb9589d
Kyle Krattiger <mr.music25@gmail.com>
2017-04-12 18:40:21 -0700

    Created Project 3 for assembly class

---
+++ ASM-Project-3/ASM-Project-3/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Prototypes for functions
+int read(int filenamelocation, int arrayaddress, int arraysize);
+void sort(int arrayaddress, int itemcount);
+int findsmallest(int arrayaddress, int start, int end);
+void swap(int arrayaddress, int a, int b);
+void print(int filenamelocation, int arrayaddress, int itemcount);
+
+int main()
+{
+	return 0;
+}
+
+void print(int filenamelocation, int arrayaddress, int itemcount)
+{
+}
